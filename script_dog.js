ae7754e61cc7498594f307a76851f6414f327c55
Eduardo <cojr.eduardo@gmail.com>
2024-02-26 22:07:22 +0100

    fjern css script i Profile card og flyttet den til css fil

--- script_dog.js
+++ script_dog.js
@@ -46,7 +46,7 @@ function profileCard(imgDog, profileUser) {
 
   const userProfileCard = document.createElement("div");
 
-  userProfileCard.innerHTML = `<img src="${dogImg}" style="width: 200px; height: auto;"/><h3>Name : ${userName}</h3><h4>Location : ${userAddress}</h4>`;
+  userProfileCard.innerHTML = `<img src="${dogImg}"/><h3>Name : ${userName}</h3><h4>Location : ${userAddress}</h4>`;
   profileCardContainer.appendChild(userProfileCard);
 }
 
