5f352b7569bc41ea178b136dbc1412e932ec9eef
Your Name <you@example.com>
2024-02-27 14:29:03 +0100

    commit

--- script_dog.js
+++ script_dog.js
@@ -24,42 +24,46 @@ console.log(fetchRandomUserProfile());
 
 //Read fetch and display profile card
 //promise.all - https://www.geeksforgeeks.org/javascript-promise-all-method/
-async function fetchAndShowProfile() {
+async function fetchAndShowProfile(index) {
   try {
     const [imgDog, profileUser] = await Promise.all([
       fetchRandomDogImage(),
       fetchRandomUserProfile(),
     ]);
 
-    profileCard(imgDog, profileUser);
+    profileCard(imgDog, profileUser, index);
 
-    console.log("Inside fetch and show profile function", imgDog, profileUser);
+    console.log("Inside fetch and show profile function", imgDog, profileUser, index);
   } catch (error) {
     console.error("Unable to load data", error);
   }
 }
 
-function profileCard(imgDog, profileUser) {
+function profileCard(imgDog, profileUser, index) {
   const dogImg = `${imgDog.message}`;
   const userName = `${profileUser.name.first} ${profileUser.name.last}`;
   const userAddress = `${profileUser.location.city}, ${profileUser.location.country}`;
-  const deleteBtn = document.createElement('button');
+
+  const deleteBtn = document.createElement('button')
   deleteBtn.innerHTML = 'Delete'
   deleteBtn.style.backgroundColor = 'red'
   const userProfileCard = document.createElement("div");
   userProfileCard.innerHTML = `<img src="${dogImg}"/><h3>Navn : ${userName}</h3><h4>Bosted : ${userAddress}</h4>`;
-  profileCardContainer.appendChild(userProfileCard);
+  const existingCard = profileCardContainer.children[index];
+  profileCardContainer.insertBefore(userProfileCard, existingCard)
+  userProfileCard.appendChild(deleteBtn)
+  console.log( 'inne i profileCard funksjonen', existingCard)
   deleteBtn.addEventListener("click", ()=> {
-    deleteCard(userProfileCard)
+    deleteCard(userProfileCard, index)
   })
-  userProfileCard.append(deleteBtn)
+  
 }
 
 async function showProfileCards() {
   profileCardContainer.innerHTML = "";
 
   for (let i = 0; i < 10; i++) {
-    await fetchAndShowProfile();
+    await fetchAndShowProfile(i); //passing the index to the fetchAndShowProfile to identfy positioning
   }
 }
 
@@ -67,7 +71,7 @@ showProfileCards();
 showBtn.onclick = showProfileCards;
 
 // Delete and replace
-const deleteCard = async(userProfileCard) =>{
+const deleteCard = async(userProfileCard, index) =>{
   userProfileCard.remove()
-  await fetchAndShowProfile()
+  await fetchAndShowProfile(index)
 }
