82091df65deeedd71b61fed1f0deb0d18f56fb14
Your Name <you@example.com>
2024-03-02 17:02:35 +0100

    dogSays ferdig

--- script_dog.js
+++ script_dog.js
@@ -48,6 +48,10 @@ function profileCard(imgDog, profileUser, index) {
   const userName = `${profileUser.name.first} ${profileUser.name.last}`;
   const userAddress = `${profileUser.location.city}, ${profileUser.location.country}`;
 
+  const dogChat = document.createElement('div')
+  dogChat.style.display = "none"
+  
+
   const deleteBtn = document.createElement("button");
   deleteBtn.className = "delete-btn";
   deleteBtn.innerHTML = "Delete";
@@ -66,13 +70,14 @@ function profileCard(imgDog, profileUser, index) {
 
   const userProfileCard = document.createElement("div");
   userProfileCard.className = "profile-card";
-  userProfileCard.innerHTML = `<img src="${dogImg}"/><h3>Navn : ${userName}</h3><h4>Bosted : ${userAddress}</h4>`;
+  userProfileCard.innerHTML = `<img src="${dogImg}"/><h3>Navn : ${userName}</h3><h4>Bosted : ${userAddress}</h4>`
 
   const existingCard = profileCardContainer.children[index];
   profileCardContainer.insertBefore(userProfileCard, existingCard);
   userProfileCard.appendChild(deleteBtn);
   userProfileCard.appendChild(chatBtn);
   userProfileCard.appendChild(chatBox);
+  userProfileCard.appendChild(dogChat);
 
   deleteBtn.addEventListener("click", () => {
     deleteCard(userProfileCard, index);
@@ -90,6 +95,13 @@ function profileCard(imgDog, profileUser, index) {
   sendBtn.addEventListener("click", () => {
     messageSend(chatBox);
   });
+
+  userProfileCard.addEventListener("click", (e) => {
+    if (e.target.tagName === 'IMG') {
+      showDogChat(dogChat);
+    }
+  });
+  
 }
 
 function showChatBox(chatBox) {
@@ -175,6 +187,14 @@ const fetchBreed = async (breed) => {
   }
 };
 
+const dogArray = ["Voff voff", "Grrr!", "Mjau??", "Voff!", "Voff voff voff", "WRAFF!!!"];
+
+const dogSays = () => {
+  const randomIndex = Math.floor(Math.random() * dogArray.length);
+  return dogArray[randomIndex];
+}
+
+
 const displayCard = (imageUserPairs) => {
   profileCardContainer.innerHTML = "";
   imageUserPairs.forEach((pair) => {
@@ -185,24 +205,32 @@ const displayCard = (imageUserPairs) => {
     cardContainer.className = "profile-card";
     const userName = document.createElement("h3");
     const userAddress = document.createElement("h4");
+    const dogChat = document.createElement('div')
+    dogChat.style.display = "none"
 
     userName.innerHTML = `Name: ${pair.profile.name.first} ${pair.profile.name.last}`;
     userAddress.innerHTML = `Location: ${pair.profile.location.city}, ${pair.profile.location.country}`;
     userProfile.append(userName, userAddress);
-    cardContainer.append(img, userProfile);
-    profileCardContainer.appendChild(cardContainer);
+    cardContainer.append(img, userProfile, dogChat);
+    profileCardContainer.append(cardContainer);
+
+    // Add event listener to the card container
+    img.addEventListener("click", () => {
+      showDogChat(dogChat)
+    });
   });
 };
 
-const dogArray = ["Voff voff", "Grrr!", "Mjau??", "Voff!", "Voff voff voff", "WRAFF!!!"];
+function showDogChat(dogChat) {
+  dogChat.style.display = "block";
+  dogChat.innerHTML = dogSays();
+  dogChat.style.backgroundColor = "white";
+  dogChat.style.padding = "10px";
+  dogChat.style.borderRadius = "100px";
+  dogChat.style.textAlign = "center"; 
 
-const dogSays = () => {
-  const randomIndex = Math.floor(Math.random() * dogArray.length);
-  return dogArray[randomIndex];
+  setTimeout(() => {
+    dogChat.style.display = "none";
+  }, 1000); 
 }
 
-console.log(dogSays())
-
-const div = document.querySelector('div').addEventListener("click", ()=>{
-  alert(dogSays())
-})
